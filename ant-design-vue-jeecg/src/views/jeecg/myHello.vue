af6fc6d922c80e98eab8b3cdb10de80471a595f0
xxm <934792834@qq.com>
2019-09-19 22:32:33 +0800

    vue

--- ant-design-vue-jeecg/src/views/jeecg/myHello.vue
+++ ant-design-vue-jeecg/src/views/jeecg/myHello.vue
@@ -1,29 +1,47 @@
 <template>
-  <div>{{msg}}</div>
+  <!--页面展示区域-->
+  <div>
+    {{msg}},{{mm}}
+    <p v-if="bol">{{s}}</p>
+    <p v-for="i in list">{{i.v}}</p>
+
+  </div>
 </template>
 
 <script>
   export default {
-    name: "myHello"
-    , data() {
-      return {msg: ''}
-    }
-    , methods: {
+    name: "myHello",
+    //数据
+    data() {
+      return {
+        msg: '',
+        mm: '',
+        bol: true,
+        s: 'value-test',
+        list: [{'v': 1}, {'v': 2}, {'v': 3}, {'v': 4}]
+      }
+    },
+    //方法调用
+    methods: {
       yw() {
         var url = '/yw/ywTest'
         getAction(url).then((res) => {
           if (res.success) {
+            console.log("-----------log test -----------")
             this.msg = res.result
+            this.mm = res.result
           }
         })
       }
     },
+    //页面初始化的时候就执行   dom对象还没有加载完毕
     created() {
+      //也可以直接在这里调用getAction()
       this.yw();
     }
   }
-  import {getAction} from "../../api/manage";
-</script>
+  //发送请求的方法   或者 @/api/manage
+  import {getAction} from "../../api/manage";</script>
 
 <style scoped>
 
