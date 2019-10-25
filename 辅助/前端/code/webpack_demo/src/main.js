17086b8b02a61cada4412ed853dc48ae97f49fe0
xxm <934792834@qq.com>
2019-10-25 23:22:47 +0800

    webpack

---
+++ 辅助/前端/code/webpack_demo/src/main.js
@@ -0,0 +1,17 @@
+//入口js
+
+//导入jquery--$:变量名;jquery:nodel_model中的jquery模块
+//import ** from ** 是es6导入模块的方式
+import $ from 'jquery'
+
+//隔行变色
+$(function () {
+    //odd偶数，even奇数
+    $('li:odd').css('backgroundColor', 'lightblue')
+    $('li:even').css('backgroundColor', function () {//另一种方式
+        return '#' + 'D97634'
+    })
+})
+
+//webpack：1 能够处理js文件相互依赖关系 2 能够处理js兼容问题，把高级的浏览器不支持的语法(es6),转为低级的，浏览器能识别的语法
+//webpack xxxx 打包到dist默认位置
---
+++ 辅助/前端/code/webpack_demo/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+
+//node语法，webpack基于node
+//这个配置文件，通过node中的模块操作，向外暴露一个配置对象
+module.exports = {
+    entry: path.join(__dirname, './src/main.js'),//入口，打那个包
+    output: {
+        path: path.join(__dirname, './src/dist'),
+        filename: 'main.js'
+    }
+}
+//直接运行webpack打包
